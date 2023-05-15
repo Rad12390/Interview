11ab23be743b9a55461d3da70085ad7152f99734
radhika1590 <radhikarani1590@gmail.com>
2023-05-15 10:56:34 +0530

    interview wordpress

---
+++ wp-content/themes/twentytwentythree/function.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Three
+ * @since Twenty Twenty-Three 1.0
+ */
+
+
+
+?>
